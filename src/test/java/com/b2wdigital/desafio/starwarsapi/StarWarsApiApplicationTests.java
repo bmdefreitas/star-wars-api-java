ca2bf6f6cf90f79440ae4a7322b1260c921823ab
Bruno <bmdefreitas@gmail.com>
2020-03-11 20:43:44 -0300

    Iniciando o projeto Star Wars API em Java com Spring Boot

---
+++ src/test/java/com/b2wdigital/desafio/starwarsapi/StarWarsApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.b2wdigital.desafio.starwarsapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StarWarsApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
